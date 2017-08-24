7ac1f2624b84ca4da4fa8a3b73040be0c4ba4d2b
Henrik Loevborg <hlo@miracle.dk>
2017-08-24 07:58:57 +0200

    added esdh-label to dto class for import

--- sources/TopicRouter/src/main/java/dk/os2opgavefordeler/orgunit/EmployeeDTO.java
+++ sources/TopicRouter/src/main/java/dk/os2opgavefordeler/orgunit/EmployeeDTO.java
@@ -6,38 +6,42 @@ import javax.xml.bind.annotation.XmlElement;
 
 public class EmployeeDTO {
 
-    public String businessKey;
-    public String name;
-    public String email;
-    public String initials;
-    public String jobTitle;
-    public String phone;
-    public String esdhId;
+	public String businessKey;
+	public String name;
+	public String email;
+	public String initials;
+	public String jobTitle;
+	public String phone;
+	public String esdhId;
+	public String esdhLabel;
 
-    public EmployeeDTO(){}
+	public EmployeeDTO() {
+	}
 
-    public EmployeeDTO(Employment fromEmployment){
-        if (fromEmployment != null) {
-            businessKey = fromEmployment.getBusinessKey();
-            name = fromEmployment.getName();
-            email = fromEmployment.getEmail();
-            initials = fromEmployment.getInitials();
-            jobTitle = fromEmployment.getJobTitle();
-            phone = fromEmployment.getPhone();
-            esdhId = fromEmployment.getEsdhId();
-        }
-    }
+	public EmployeeDTO(Employment fromEmployment) {
+		if (fromEmployment != null) {
+			businessKey = fromEmployment.getBusinessKey();
+			name = fromEmployment.getName();
+			email = fromEmployment.getEmail();
+			initials = fromEmployment.getInitials();
+			jobTitle = fromEmployment.getJobTitle();
+			phone = fromEmployment.getPhone();
+			esdhId = fromEmployment.getEsdhId();
+			esdhLabel = fromEmployment.getEsdhLabel();
+		}
+	}
 
-    @Override
-    public String toString() {
-        return "EmployeeDTO{" +
-                "businessKey='" + businessKey + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", initials='" + initials + '\'' +
-                ", jobTitle='" + jobTitle + '\'' +
-                ", phone='" + phone + '\'' +
-                ", esdhId='" + esdhId + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "EmployeeDTO{" +
+				"businessKey='" + businessKey + '\'' +
+				", name='" + name + '\'' +
+				", email='" + email + '\'' +
+				", initials='" + initials + '\'' +
+				", jobTitle='" + jobTitle + '\'' +
+				", phone='" + phone + '\'' +
+				", esdhId='" + esdhId + '\'' +
+				", esdhLabel='" + esdhLabel+ '\'' +
+				'}';
+	}
 }
